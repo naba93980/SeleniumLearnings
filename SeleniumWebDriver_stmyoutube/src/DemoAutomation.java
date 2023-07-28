c184e04aacf91cbddcf8b35d722ea78b027d5af0
= <=>
2023-07-28 17:15:59 +0530

    browser driver instance

---
+++ SeleniumWebDriver_stmyoutube/src/DemoAutomation.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		ChromeDriver chromeDriver = new ChromeDriver();
+		chromeDriver.get("https://www.facebook.com");
+		
+		EdgeDriver edgeDriver = new EdgeDriver();
+		edgeDriver.get("https://www.youtube.com");
+	}
+
+}
