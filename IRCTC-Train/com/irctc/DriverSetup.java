15ac751ad9bc451f50b1dce9c980ee879be4c955
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-08-01 02:20:29 +0530

    updated with seat class

--- IRCTC-Train/com/irctc/DriverSetup.java
+++ IRCTC-Train/com/irctc/DriverSetup.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverSetup {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// WebDriver driver=new EdgeDriver();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.irctc.co.in/nget/train-search");
--- IRCTC-Train/com/irctc/TraverseSite.java
+++ IRCTC-Train/com/irctc/TraverseSite.java
@@ -15,54 +15,54 @@ public class TraverseSite {
 	public static WebElement dateElement;
 	public static WebElement classElement;
 
-	public static void source(WebDriver driver) {
+	public static void source(WebDriver driver) throws InterruptedException {
 		source = driver.findElement(By.cssSelector("input.ng-tns-c57-8"));
 		source.sendKeys("Hyd");
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+
+		Thread.sleep(2000);
+		
 		source.sendKeys(Keys.ARROW_DOWN);
 		source.sendKeys(Keys.ENTER);
 	}
 
-	public static void destination(WebDriver driver) {
+	public static void destination(WebDriver driver) throws InterruptedException {
 		destination = driver.findElement(By.cssSelector("input.ng-tns-c57-9"));
 		destination.sendKeys("Pune");
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		
+		Thread.sleep(2000);
+
 		destination.sendKeys(Keys.ENTER);
 	}
 
-	public static void date(WebDriver driver) {
+	public static void date(WebDriver driver) throws InterruptedException {
 
 		dateElement = driver.findElement(By.cssSelector("input.ng-tns-c58-10"));
 		String currentDate = dateElement.getAttribute("value");
 		DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		LocalDate date = LocalDate.parse(currentDate, myFormatter);
 		String newDate = date.plusDays(4).format(myFormatter);
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
+		
+		Thread.sleep(3000);
+		
 		for (int i = 0; i < newDate.length(); i++) {
 			dateElement.sendKeys(Keys.BACK_SPACE);
 		}
+
+		Thread.sleep(1000);
+
 		dateElement.sendKeys(newDate);
 		driver.findElement(By.className("ui-state-active")).click();
 
 	}
 
-	public static void journeyClass(WebDriver driver) {
+	public static void journeyClass(WebDriver driver) throws InterruptedException {
+
 		classElement=driver.findElement(By.cssSelector(".ui-clickable.ng-tns-c65-11"));
+
+		Thread.sleep(3000);
+
 		classElement.click();
-		// System.out.println(driver.findElement(By.tagName("#journeyClass > div > div.ng-trigger.ng-trigger-overlayAnimation.ng-tns-c65-11.ui-dropdown-panel.ui-widget.ui-widget-content.ui-corner-all.ui-shadow.ng-star-inserted > div > ul > p-dropdownitem:nth-child(12) > li")));
+		driver.findElement(By.cssSelector("#journeyClass > div > div.ng-trigger.ng-trigger-overlayAnimation.ng-tns-c65-11.ui-dropdown-panel.ui-widget.ui-widget-content.ui-corner-all.ui-shadow.ng-star-inserted > div > ul > p-dropdownitem:nth-child(12) > li")).click();
 		
 	}
 }
