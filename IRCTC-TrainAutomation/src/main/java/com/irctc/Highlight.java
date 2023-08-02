2b8f5859120d0ea50626ff8e4e8fdd04d676ac19
NabajyotiModak <naba93981@gmail.com>
2023-08-02 19:02:14 +0530

    class to highlight current web element using javascript executor

---
+++ IRCTC-TrainAutomation/src/main/java/com/irctc/Highlight.java
@@ -0,0 +1,17 @@
+package com.irctc;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Highlight {
+	public static void addHighlight(WebDriver driver, WebElement element) throws InterruptedException {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid black');", element);
+	}
+
+	public static void removeHighlight(WebDriver driver, WebElement element) throws InterruptedException {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('style','border: solid 2px black');", element);
+	}
+}
