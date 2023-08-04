3efe32249ea5e5850eddfa0d8c14aac8f24f484c
NabajyotiModak <naba93981@gmail.com>
2023-08-04 17:55:06 +0530

    updated

--- IRCTC-TrainAutomation/src/main/java/com/irctc/Main.java
+++ IRCTC-TrainAutomation/src/main/java/com/irctc/Main.java
@@ -2,19 +2,24 @@ package com.irctc;
 
 import org.openqa.selenium.WebDriver;
 
+import com.irctc.train.TrainDetails;
+import com.irctc.train.TraverseSite;
+import com.irctc.utils.ReadInput;
 import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args) throws Exception {
 
 		System.out.println("Enter C for Google Chrome");
 		System.out.println("Enter E for Microsoft Edge");
 		Scanner sc = new Scanner(System.in);
-		char browser =  sc.next().charAt(0);
+		char browser = sc.next().charAt(0);
 		sc.close();
 
 		WebDriver driver = DriverSetup.setDriver(browser);
-		driver.get("https://www.irctc.co.in/nget/train-search");
+		String url=ReadInput.urlInput();
+		driver.get(url);
 		driver.manage().window().maximize();
 		
 		CheckWebsite.verifyTitle(driver);
@@ -25,6 +30,5 @@ public class Main {
 		TraverseSite.disabilityConcession(driver);
 		TraverseSite.findTrains(driver);
 		TrainDetails.trainsAvailablity(driver);
-		ScreenShots.Screenshot(driver);
 	}
 }
