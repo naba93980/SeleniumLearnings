018c350f2583c0ee797e5b2f10941c570439dd8e
NabajyotiModak <naba93981@gmail.com>
2023-08-29 17:46:04 +0530

    carhome page object

---
+++ TravelInsuranceAutomation/src/test/java/com/policybazaar/pages/carinsurance/CarHome.java
@@ -0,0 +1,16 @@
+package com.policybazaar.pages.carinsurance;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CarHome {
+	
+	@FindBy(xpath = "//a[text()='Buying a new car? Click here']")
+	private WebElement buyNewCar;
+	
+	public CarHome(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+}
