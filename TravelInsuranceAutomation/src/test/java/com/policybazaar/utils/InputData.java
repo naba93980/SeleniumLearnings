298e5a9527db2b0ad9094545283e4ad99b3e4ca9
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-09-03 23:33:27 +0530

    cellvalue to be formatted as string

--- TravelInsuranceAutomation/src/test/java/com/policybazaar/utils/InputData.java
+++ TravelInsuranceAutomation/src/test/java/com/policybazaar/utils/InputData.java
@@ -3,52 +3,62 @@ package com.policybazaar.utils;
 import java.util.Map;
 import java.util.HashMap;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class InputData {
 
-	public static XSSFWorkbook inputWorkbook;
-	public static XSSFSheet inputSheet;
-	public static XSSFRow inputRow;
-	public static String cellValue;
-	public static int totalRows;
+	public  static XSSFWorkbook inputWorkbook;
+	private static XSSFSheet inputSheet;
+	private static XSSFCell inputCell;
+	private static int totalRows;
 
 	public static Map<String, String> getTravelInsuranceData() {
 
 		Map<String, String> travelInsuranceData = new HashMap<>();
+		DataFormatter dataFormatter = new DataFormatter();
+		String key, value;
 
 		inputSheet = inputWorkbook.getSheet("Travel");
 		totalRows = inputSheet.getPhysicalNumberOfRows();
-		String key, value;
 
 		for (int i = 0; i < totalRows; i++) {
 
-			key = inputSheet.getRow(i).getCell(0).getStringCellValue();
-			value = inputSheet.getRow(i).getCell(1).getStringCellValue();
-
-			travelInsuranceData.put(key, value);
+			inputCell = inputSheet.getRow(i).getCell(0);
+			key = dataFormatter.formatCellValue(inputCell);
 			
+			inputCell = inputSheet.getRow(i).getCell(1);
+			value = dataFormatter.formatCellValue(inputCell);
+		
+			travelInsuranceData.put(key, value);
 		}
+		
 		return travelInsuranceData;
 	}
 
 	public static Map<String, String> getCarInsuranceData() {
 
 		Map<String, String> carInsuranceData = new HashMap<>();
+		DataFormatter dataFormatter = new DataFormatter();
+		String key, value;
 
 		inputSheet = inputWorkbook.getSheet("Car");
 		totalRows = inputSheet.getPhysicalNumberOfRows();
-		String key, value;
 
 		for (int i = 0; i < totalRows; i++) {
 
-			key = inputSheet.getRow(i).getCell(0).getStringCellValue();
-			value = inputSheet.getRow(i).getCell(1).getStringCellValue();
+			inputCell = inputSheet.getRow(i).getCell(0);
+			key = dataFormatter.formatCellValue(inputCell);
 			
+			inputCell = inputSheet.getRow(i).getCell(1);
+			value = dataFormatter.formatCellValue(inputCell);
+		
 			carInsuranceData.put(key, value);
 		}
+		
 		return carInsuranceData;
 	}
 }
