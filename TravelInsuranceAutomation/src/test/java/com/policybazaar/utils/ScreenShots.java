939e32d18b221fedd070bcf52357ced96f08e360
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-09-03 17:21:33 +0530

    updated

--- TravelInsuranceAutomation/src/test/java/com/policybazaar/utils/ScreenShots.java
+++ TravelInsuranceAutomation/src/test/java/com/policybazaar/utils/ScreenShots.java
@@ -2,37 +2,46 @@ package com.policybazaar.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Properties;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.io.FileHandler;
+
+import com.aventstack.extentreports.ExtentTest;
 
 // class to take screenshots
 public class ScreenShots {
 
+	private static WebDriver driver;
+
+	public static void setDriver(WebDriver driver) {
+		ScreenShots.driver = driver;
+	}
+
 	public static String getScreenshotLocation() {
 		Properties properties = ReadProperties.getProperties();
 		String screenshotLocation = properties.getProperty("screenshotLocation");
-		return screenshotLocation; 
+		return screenshotLocation;
 	}
 
-	public static void takeScreenshot(WebDriver driver, String fileName) throws IOException {
+	public static void takeScreenshot(String fileName, ExtentTest test) throws IOException {
 
 		// type casting to ScreenShots interface
 		TakesScreenshot screenShot = ((TakesScreenshot) driver);
 
 		// creates the raw source file
-		File Source = screenShot.getScreenshotAs(OutputType.FILE);
+		File sourceFile = screenShot.getScreenshotAs(OutputType.FILE);
 
 		// creates output destination file
 		String filePath = getScreenshotLocation() + fileName + ".png";
-		File Destination = new File(filePath);
-
+		File destinationFile = new File(filePath);
+		
 		// copies file to destination file
-		FileHandler.copy(Source, Destination);
-		System.out.println("Screenshot has been taken!");
+		Files.copy(sourceFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		
+		test.addScreenCaptureFromPath(filePath);
 	}
 }
-
